0726f9b33ad22fd20f81566053d9a1ab665ee5f7
MarcoAlvarez790 <quintero_3333@hotmail.com>
2013-10-27 01:10:18 -0600

    Create json.php

---
+++ json.php
@@ -0,0 +1,19 @@
+<?php
+	//con=mysqli_connect("localhost","root","","VSpace");
+	$con=mysqli_connect("localhost","fondeand","Parasito#666","fondeand_vspace");
+	if (mysqli_connect_errno())
+  	{
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	}
+	if($result = mysqli_query($con,"SELECT * FROM points;"))
+	{
+		while($row = $result->fetch_array(MYSQLI_ASSOC)){
+			$array[] = array("id" => $row["id"], "latitude" => $row["latitude"], "longitude" => $row["longitude"], "feeling" => $row["feeling"], "message" => $row["message"]);
+		}
+		printf(json_encode(array("dataArray" => $array)));
+	}
+	else{
+		printf("Error");
+	}
+	mysqli_close($con);
+?>
