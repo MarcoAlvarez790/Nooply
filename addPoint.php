ffa0a9656f0857dd20ec94674e2c8198849b8255
MarcoAlvarez790 <quintero_3333@hotmail.com>
2013-10-27 01:10:48 -0600

    Create addPoint.php

---
+++ addPoint.php
@@ -0,0 +1,21 @@
+<?php
+	$f = $_POST['feeling'];
+	$lat = $_POST['latitude'];
+	$lng = $_POST['longitude'];
+	$msg = $_POST['message'];
+	//$con=mysqli_connect("localhost","root","","VSpace");
+	$con=mysqli_connect("localhost","fondeand","Parasito#666","fondeand_vspace");
+	// Check connection
+	if (mysqli_connect_errno())
+  	{
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  	}
+	if(mysqli_query($con,"INSERT INTO points (feeling, latitude, longitude, message) VALUES ('$f', '$lat', '$lng', '$msg')"))
+	{
+		printf("You have shared your mood!");
+	}
+	else{
+		printf("Error");
+	}
+	mysqli_close($con);
+?>
